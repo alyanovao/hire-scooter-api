c04bcea67bd24713dc0c4492d3d6eb3912af2a76
Альянов Артем Олегович <domovoy175@gmail.com>
2022-08-26 02:07:25 +0300

    first commit

---
+++ src/test/java/ru/aao/hirescooterapi/HireScooterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aao.hirescooterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireScooterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
